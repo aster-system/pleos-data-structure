eda319c3612148c07100de58312db97b5e80315f
Matt Mmo <mmattmmo@gmail.com>
2024-09-08 16:43:44 +0200

    Add basic files

---
+++ sources/main.cpp
@@ -0,0 +1,48 @@
+//******************
+//
+// main.cpp
+//
+//******************
+// Presentation :
+//
+// PLEOS, by Aster System, is a project which aims education.
+// By using the power of SCLS and other Aster System's tools, we created  this.
+// We want to make education easier for everyone (teachers, as students and pupils).
+// The software is made in french, because the main goal is France educational system.
+// For more information, see : https://aster-system.github.io/aster-system/projects/pleos.html.
+//
+// The "Data Structure" part aims IT lesson.
+// Its goal is to explain data structure easily for everyone.
+//
+// This file contains the main part of the prototype software.
+//
+//******************
+//
+// License (GPL V3.0) :
+//
+// Copyright (C) 2024 by Aster System, Inc. <https://aster-system.github.io/aster-system/>
+// This file is part of PLEOS.
+// PLEOS is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+// PLEOS is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License along with PLEOS. If not, see <https://www.gnu.org/licenses/>.
+//
+
+// Include SCLS Graphic Benoit
+#include "../../../scls-graphic-benoit/scls_graphic.h"
+
+// Init SCLS
+SCLS_MATH_INIT
+
+// Main function of the program
+int main(int argc, char* argv[]) {
+    scls::Window window(900, 600, argv[0]);
+
+    while(window.run()) {
+        window.update_event();
+        window.update();
+
+        window.render();
+    }
+
+    return 0;
+}
